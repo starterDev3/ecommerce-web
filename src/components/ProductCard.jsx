46b0b212f119aa7b3b6600f6e7d62d23a63d8a44
starterDev3 <crazyfam@outlook.com>
2026-04-11 16:21:22 -0500

    changes

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function ProductCard({ product }) {
+    return (
+        <div className="home-products-grid">
+         <div className="home-product-card">
+            <img className="home-product-image" src={product.image} alt={product.name} />
+                <h3>{product.name}</h3>
+                <p className="product-desc">{product.description}</p>
+                <p className="card-price">${product.price.toFixed(2)}</p>
+            <Link to={`/products/${product.id}`} className="btn-normal">View Details</Link>
+        <button className="btn-cart">Add to Cart</button>
+      </div>
+    </div>
+    );
+}
---
+++ src/data/product.js
@@ -0,0 +1,70 @@
+const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        description: "test",
+        price: 1.99,
+        image: "/"
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        description: "the real",
+        price: 29.99,
+                image: "/"
+    },
+        {
+        id: 3,
+        name: "Product 3",
+        description: "etrhertgrhghtyrj",
+        price: 19.99,
+        image: "/"
+    },
+            {
+        id: 4,
+        name: "Home Stereo System",
+        description: "high-quality home stereo system with Bluetooth connectivity and powerful sound.",
+        price: 199.99,
+        image: "https://img.av-connection.com/2/AVimg_59292.jpg"
+    },
+            {
+        id: 5,
+        name: "Godzilla Plush Toy",
+        description: "soft and cuddly Godzilla plush toy, perfect for fans of the iconic monster.",
+        price: 2,
+        image: "https://media.entertainmentearth.com/assets/images/faed5f0053324052b10ef4b5fdeedffdlg.jpg"
+    },
+            {
+        id: 6,
+        name: "M-MINIGAMES!?!?!?",
+        description: "expendable on door 30",
+        price: 999999.99,
+        image: "https://i.ytimg.com/vi/DcAfVjyJIB8/maxresdefault.jpg"
+    },
+                {
+        id: 7,
+        name: "Product 7",
+        description: "etrhertgrhghtyrj",
+        price: 59.99,
+        image: "/"
+    },
+            {
+        id: 8,
+        name: "50 mini-fridges that i found in the dumpster behind my apartment",
+        description: "look, i know this is hella expensive, but we're kinda broke right now so I'd REALLY appreciate it if you bought this epic mini-fridge.",
+        price: 999.99,
+        image: "https://ewscripps.brightspotcdn.com/dims4/default/bbd04af/2147483647/strip/true/crop/1280x672+0+24/resize/1200x630!/quality/90/?url=https%3A%2F%2Fcf.cdn.uplynk.com%2Fausw%2Fslices%2F1db%2Fef205c0e5ea14d77944cbd6904335118%2F1dbbb8813b894e8792cd456ae92745cf%2Fposter_22565a99219c43139a7e2b9a54f7a8ba.jpg"
+    },
+            {
+        id: 9,
+        name: "Product 9",
+        description: "n o",
+        price: 3.99,
+        image: "/"
+    },
+];
+
+
+export function Getproducts() {
+    return products;
+}
